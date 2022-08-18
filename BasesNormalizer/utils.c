f55ba3718a1e8e2a9c2a754aa08e1c46f9a87915
Theodikes <gvedichi@gmail.com>
2022-08-18 22:19:20 +0300

    Перенёс часть функций, которые могут быть использованы не только в нормализаторе, в utils

---
+++ BasesNormalizer/utils.c
@@ -0,0 +1,56 @@
+﻿#include "utils.h"
+
+bool startsWith(const char* str, const char* prefix)
+{
+	size_t lenpre = strlen(prefix),
+		lenstr = strlen(str);
+	return lenstr < lenpre ? false : memcmp(prefix, str, lenpre) == 0;
+}
+
+bool endsWith(const char* str, const char* suffix)
+{
+	if (!str || !suffix)
+		return 0;
+	size_t lenstr = strlen(str);
+	size_t lensuffix = strlen(suffix);
+	if (lensuffix > lenstr)
+		return 0;
+	return strncmp(str + lenstr - lensuffix, suffix, lensuffix) == 0;
+}
+
+char* path_join(const char* dir, const char* file) {
+
+	size_t size = strlen(dir) + strlen(file) + 2;
+	char* buf = (char*)malloc(size * sizeof(char));
+	if (NULL == buf) return NULL;
+
+	strcpy(buf, dir);
+
+	// add the sep if necessary
+	if (!endsWith(dir, PATH_JOIN_SEPARATOR)) {
+		strcat(buf, PATH_JOIN_SEPARATOR);
+	}
+
+	// remove the sep if necessary
+	if (startsWith(file, PATH_JOIN_SEPARATOR)) {
+		char* filecopy = _strdup(file);
+		if (NULL == filecopy) {
+			free(buf);
+			return NULL;
+		}
+		strcat(buf, ++filecopy);
+		free(--filecopy);
+	}
+	else {
+		strcat(buf, file);
+	}
+
+	return buf;
+}
+
+char* getFilenameFromPath(char* pathToFile) {
+	char* fileNameWithExtension;
+	(fileNameWithExtension = strrchr(pathToFile, '\\')) ? ++fileNameWithExtension : (fileNameWithExtension = pathToFile);
+
+	return fileNameWithExtension;
+}
---
+++ BasesNormalizer/utils.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+#ifndef MY_UTILS
+#define MY_UTILS
+
+#include <stdio.h>
+#include <string.h>
+#include <malloc.h>
+#include <stdbool.h>
+#include "libs/argparse/argparse.h" // https://github.com/cofyc/argparse
+#include "libs/tiny-regex/re.h" // https://github.com/kokke/tiny-regex-c
+#include <Windows.h>
+
+// Разделители путей в различных системах
+#ifdef _WIN32
+#define PATH_JOIN_SEPARATOR   "\\"
+#else
+#define PATH_JOIN_SEPARATOR   "/"
+#endif
+
+// Переменная для небольших положительных числовых значений, влезающих в байт памяти
+#define ushortest unsigned char
+// Максимальная длина пути к файлу в Windows
+#define MAX_PATH 1024
+
+// Проверяет, начинается ли строка с определённой другой строки (например, startsWith("test", "testing") == true)
+bool startsWith(const char* pre, const char* str);
+
+// Проверяет, заканчивается ли строка определённой другой строкой (например, endsWith("testing", "ing") == true)
+bool endsWith(const char* str, const char* suffix);
+
+// Функция для объединения абсолютного пути к папке и имени файла в абсолютный путь к файлу. Возвращает итоговый путь
+char* path_join(const char* dir, const char* file);
+
+// Функция принимает в качестве аргумента валидный путь к файлу и возвращает имя файла (с расширением)
+char* getFilenameFromPath(char* pathToFile);
+
+#endif // !MY_UTILS
